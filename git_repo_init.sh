2a1a070af781cf91e79ae93c9295915967af9e32
Christian Hitz <christian@klarinett.li>
2015-03-30 20:58:56 +0200

    restructure init script

--- git_repo_init.sh
+++ git_repo_init.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-SVN_GIT_DIR="scribus_svn4"
-CONFIG_DIR=$(pwd)
+if [ ! -e ".git/config" ]
+then
+    echo "$0 must be run at the root of an empty git repository."
+    exit 1
+fi
 
-git init $SVN_GIT_DIR
-pushd $SVN_GIT_DIR
-
-cat >> .git/config <<EOF
+git svn status 2> /dev/null
+if [ $(grep -c "svn-remote" .git/config) -eq 0 ]
+then
+    cat >> .git/config <<EOF
 [svn-remote "svn"]
         url = svn://scribus.net
         fetch = trunk/Scribus:refs/heads/master
 	branches = branches/*/Scribus:refs/heads/*
 	tags = tags/*/Scribus:refs/remotes/svn/tags/*
 [svn]
-        authorsfile = $CONFIG_DIR/svn_authors.txt
+        authorsfile = $(dirname $0)/svn_authors.txt
 EOF
+else
+    echo "Repository already contains a git svn configuration. Not changing anything."
+fi
